23d5bb55170a978dcd7c96659729acbb706a9f89
Bruno Haetinger <brunohaetinger@gmail.com>
2025-08-12 23:03:37 -0300

    Fix To Do cli improve to do object

--- todo-cli/index.js
+++ todo-cli/index.js
@@ -8,12 +8,12 @@ program
   .description('To Do list in CLI')
   .version('0.8.0');
 
-program.command('add')
-  .description('Add new to do item')
+program.command('add <title>')
+  .description('Add new to-do item')
   .argument('<string>', 'Item title')
   .action((itemTitle, options) => {
-    todos.push(itemTitle);
-    console.log({itemTitle, options})
+    todos.push({ title: itemTitle, done: false});
+    console.log(`✅ Added: ${itemTitle}`)
   });
 
 program.parse();
