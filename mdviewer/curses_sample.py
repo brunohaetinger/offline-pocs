3112de9077b841cef5e64d313b10297350d14f90
Bruno Haetinger <brunohaetinger@gmail.com>
2025-09-04 18:54:36 +0100

    mdviewer: sample curses hide cursor

--- mdviewer/curses_sample.py
+++ mdviewer/curses_sample.py
@@ -2,6 +2,7 @@ import curses
 
 def main(stdscr):
     stdscr.clear()
+    curses.curs_set(0)
     stdscr.addstr(0, 0, "Hello from curses!")
     stdscr.addstr("\nPress any key to exit.")
     stdscr.refresh()
