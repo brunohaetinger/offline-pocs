e6ecac5a711a21ef2adf8626e9c991f6e44f48b9
Bruno Haetinger <brunohaetinger@gmail.com>
2025-09-05 17:18:47 +0100

    Curses centrilize all

--- mdviewer/curses_cample_midddle.py
+++ mdviewer/curses_cample_midddle.py
@@ -5,10 +5,11 @@ def main(stdscr):
     curses.curs_set(0)
     max_y, max_x = stdscr.getmaxyx()
     msg1 = "Hello from curses!"
-    msg2 = "\nPress any key to exit."
+    msg2 = "Press any key to exit."
     msg1_x = int((max_x/2)) - int(len(msg1)/2)
-    stdscr.addstr(int(max_y/2), int(max_x/2), msg1)
-    stdscr.addstr(msg2)
+    msg2_x = int((max_x/2)) - int(len(msg2)/2)
+    stdscr.addstr(int(max_y/2), msg1_x, msg1)
+    stdscr.addstr(int(max_y/2)+1, msg2_x,msg2)
     stdscr.refresh()
     stdscr.getch()
 
